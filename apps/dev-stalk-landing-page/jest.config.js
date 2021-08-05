75859abb9cb510eaf3f0f8add71e71aae550a4f6
madhurgoyal19 <madhurgoyal19@gmail.com>
2021-08-05 17:09:30 +0530

    Workspace Created

---
+++ apps/dev-stalk-landing-page/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'dev-stalk-landing-page',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/dev-stalk-landing-page',
+};
