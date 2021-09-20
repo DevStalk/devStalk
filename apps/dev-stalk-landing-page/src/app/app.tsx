085f7c712a6e56a7c975b2e895b4d0a88bbe83a3
madhurgoyal19 <madhurgoyal19@gmail.com>
2021-09-20 17:21:04 +0530

    Removed Cursor for Tablet and Mobile

--- apps/dev-stalk-landing-page/src/app/app.tsx
+++ apps/dev-stalk-landing-page/src/app/app.tsx
@@ -3,13 +3,15 @@ import NavigationBar from './components/navigation-bar/navigation-bar';
 import Home from './pages/home/home';
 import About from './pages/about/about';
 import CustomCursor from './components/custom-cursor/custom-cursor';
+import { useMediaQuery } from 'react-responsive';
 
 import styles from './app.module.scss';
 
 export function App() {
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1200px)' });
   return (
     <div id="app" className={styles.app}>
-      <CustomCursor />
+      {isTabletOrMobile ? null : <CustomCursor />}
       <NavigationBar />
       <Home />
       <About />
