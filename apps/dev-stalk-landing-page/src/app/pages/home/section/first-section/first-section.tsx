93e8e2de40a79040e724933e19e92a71d71d4eda
Ujjwal Goyal <madhurgoyal19@gmail.com>
2021-11-19 17:40:13 +0530

    First Section Implementation

--- apps/dev-stalk-landing-page/src/app/pages/home/section/first-section/first-section.tsx
+++ apps/dev-stalk-landing-page/src/app/pages/home/section/first-section/first-section.tsx
@@ -1,23 +1,54 @@
 import styles from './first-section.module.scss';
 import { useEffect } from 'react';
 import { gsap } from 'gsap';
-import { TextSplitting } from '@dev-stalk/utils';
 import { ScrollTrigger } from 'gsap/src/all';
 import CtaButton from '../../../../components/cta-button/cta-button';
-import { ReactComponent as Logo } from '../../../../../assets/logo/Icon-Color.svg';
-import { ReactComponent as WordMark } from '../../../../../assets/logo/Wordmark-White.svg';
+import { ReactComponent as WordMark } from '../../../../../assets/logo/art.svg';
 
 /* eslint-disable-next-line */
 export interface FirstSectionProps {}
 
 export function FirstSection(props: FirstSectionProps) {
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.' + styles.firstSection,
+        start: 'top top',
+        end: 'bottom+=900 top',
+        pin: '.' + styles.firstSection,
+        markers: true,
+        scrub: 0.5,
+      },
+    });
+
+    timeline
+      .to('.' + styles.firstSection__left, { opacity: 1, duration: 0.6 })
+      .to('.' + styles.firstSection__separator__sep, {
+        width: '60vw',
+        duration: 0.9,
+      })
+      .to('.' + styles.firstSection__right, {
+        display: 'flex',
+        duration: 0,
+      })
+      .to(
+        '.' + styles.firstSection__separator__sep,
+        {
+          width: '1px',
+          duration: 0.9,
+        },
+        1.7
+      );
+  });
   return (
     <div className={styles.firstSection}>
       <div className={styles.firstSection__left}>
         <WordMark />
       </div>
       <div className={styles.firstSection__separator}>
-        <div> </div>
+        <div className={styles.firstSection__separator__sep}> </div>
       </div>
       <div className={styles.firstSection__right}>
         <div className={styles.firstSection__right__wrapper}>
